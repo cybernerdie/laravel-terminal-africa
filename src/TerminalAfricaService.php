85b632c913c220c3788d584a41636989fed0b1f7
Joshua Paul <cybernerdie@Joshuas-MacBook-Pro.local>
2023-02-19 20:47:53 +0100

    fixed issues with missing files

--- src/TerminalAfricaService.php
+++ src/TerminalAfricaService.php
@@ -2,25 +2,25 @@
 
 namespace Cybernerdie\LaravelTerminalAfrica;
 
-use Cybernerdie\LaravelTerminalAfrica\Traits\CustomerApi;
 use GuzzleHttp\Client;
-use Cybernerdie\LaravelTerminalAfrica\Traits\RateApi;
-use Cybernerdie\LaravelTerminalAfrica\Traits\ShipmentApi;
 use Cybernerdie\LaravelTerminalAfrica\Contracts\TerminalAfricaContract;
 use Cybernerdie\LaravelTerminalAfrica\Exceptions\TerminalAfricaException;
 use Cybernerdie\LaravelTerminalAfrica\Traits\AddressTrait;
 use Cybernerdie\LaravelTerminalAfrica\Traits\CarrierTrait;
 use Cybernerdie\LaravelTerminalAfrica\Traits\ClaimTrait;
+use Cybernerdie\LaravelTerminalAfrica\Traits\CustomerTrait;
 use Cybernerdie\LaravelTerminalAfrica\Traits\InsuranceTrait;
 use Cybernerdie\LaravelTerminalAfrica\Traits\PackagingTrait;
 use Cybernerdie\LaravelTerminalAfrica\Traits\ParcelTrait;
+use Cybernerdie\LaravelTerminalAfrica\Traits\RateTrait;
+use Cybernerdie\LaravelTerminalAfrica\Traits\ShipmentTrait;
 use Illuminate\Http\Client\RequestException;
 
 class TerminalAfricaService implements TerminalAfricaContract
 {
-    use ShipmentApi,
-        RateApi,
-        CustomerApi,
+    use ShipmentTrait,
+        RateTrait,
+        CustomerTrait,
         AddressTrait,
         CarrierTrait,
         ClaimTrait,
