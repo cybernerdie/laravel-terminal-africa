3a19956c0a48fab32a620f71b775ae635329fa92
Joshua Paul <cybernerdie@Joshuas-MacBook-Pro.local>
2023-03-01 19:14:26 +0100

    created constant class for all api endpooints

---
+++ src/Constants/TerminalAfricaConstant.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Cybernerdie\LaravelTerminalAfrica\Constants;
+
+class TerminalAfricaConstant
+{
+    const ADDRESS_ENDPOINT = '/addresses';
+
+    const CARRIER_ENDPOINT = '/carriers';
+
+    const CLAIM_ENDPOINT = '/claims';
+
+    const INSURANCE_ENDPOINT = '/insurance';
+
+    const PACKAGING_ENDPOINT = '/packaging';
+
+    const PARCEL_ENDPOINT = '/parcels';
+
+    const RATE_ENDPOINT = '/rates';
+
+    const SHIPMENT_ENDPOINT = '/shipments';
+
+    const TRANSACTION_ENDPOINT = '/transactions';
+
+    const USER_ENDPOINT = '/users';
+}
