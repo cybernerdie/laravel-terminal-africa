82a7054124510f4aa31beb6a98b9726e64dcf2b0
Joshua Paul <cybernerdie@Joshuas-MacBook-Pro.local>
2023-03-01 19:03:46 +0100

    remove database from php stan

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -11,10 +11,6 @@ class TestCase extends Orchestra
     protected function setUp(): void
     {
         parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Cybernerdie\\LaravelTerminalAfrica\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
     }
 
     protected function getPackageProviders($app)
@@ -26,11 +22,5 @@ class TestCase extends Orchestra
 
     public function getEnvironmentSetUp($app)
     {
-        config()->set('database.default', 'testing');
-
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_laravel-terminal-africa_table.php.stub';
-        $migration->up();
-        */
     }
 }
